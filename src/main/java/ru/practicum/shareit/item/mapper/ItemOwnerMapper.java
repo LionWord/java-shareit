c955ee72c77f3051535c156202f54be78ad82982
vladimir <PanVlVl@yandex.ru>
2023-01-11 12:11:04 +0300

    fix: passes all booking tests

--- src/main/java/ru/practicum/shareit/item/mapper/ItemOwnerMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemOwnerMapper.java
@@ -3,12 +3,15 @@ package ru.practicum.shareit.item.mapper;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingShortDto;
 import ru.practicum.shareit.booking.mapper.BookingMapperDpa;
+import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.State;
+import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.booking.service.BookingService;
 import ru.practicum.shareit.item.dto.ItemForOwnerDto;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ItemOwnerMapper {
 
@@ -26,14 +29,26 @@ public class ItemOwnerMapper {
             return newItem;
         }
 
-        Integer lastBookingId = bookingDtoList.get(bookingDtoList.size() - 1).getId();
-        Integer lastBookingBookerId = bookingDtoList.get(bookingDtoList.size() - 1).getBooker().getId();
-        Integer nextBookingId = bookingDtoList.get(0).getId();
-        Integer nextBookingBookerId = bookingDtoList.get(0).getBooker().getId();
-        BookingShortDto lastBooking = new BookingShortDto(lastBookingId, lastBookingBookerId);
-        BookingShortDto nextBooking = new BookingShortDto(nextBookingId, nextBookingBookerId);
-        newItem.setLastBooking(lastBooking);
-        newItem.setNextBooking(nextBooking);
+        boolean containsBooking = false;
+        for (BookingDto b : bookingDtoList) {
+            if (b.getItem().getId() == item.getId()) {
+                containsBooking = true;
+                break;
+            }
+        }
+        if (containsBooking) {
+            int lastBookingId = bookingDtoList.get(bookingDtoList.size() - 1).getId();
+            int lastBookingBookerId = bookingDtoList.get(bookingDtoList.size() - 1).getBooker().getId();
+            int nextBookingId = bookingDtoList.get(0).getId();
+            int nextBookingBookerId = bookingDtoList.get(0).getBooker().getId();
+            BookingShortDto lastBooking = new BookingShortDto(lastBookingId, lastBookingBookerId);
+            BookingShortDto nextBooking = new BookingShortDto(nextBookingId, nextBookingBookerId);
+            newItem.setLastBooking(lastBooking);
+            newItem.setNextBooking(nextBooking);
+            return newItem;
+        }
+        newItem.setLastBooking(null);
+        newItem.setNextBooking(null);
         return newItem;
 
 
