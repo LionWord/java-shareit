5cbe1bb2c415c89c5fe39466ac93346a245b7bd8
Vladimir Panchurin <PanVIVI@yandex.ru>
2023-01-24 23:14:55 +0300

    feature: implement userService integrationTests

--- src/test/java/ru/practicum/shareit/item/service/IntegrationItemServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/item/service/IntegrationItemServiceImplTest.java
@@ -2,11 +2,9 @@ package ru.practicum.shareit.item.service;
 
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
