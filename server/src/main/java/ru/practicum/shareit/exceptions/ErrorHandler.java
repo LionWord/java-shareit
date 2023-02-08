b2ae837e3ae80d33d57048bb6e7523fecf6230f4
LionWord <PanVlVl@yandex.ru>
2023-02-08 16:57:49 +0300

    Checking tests

--- server/src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
-import org.postgresql.util.PSQLException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
